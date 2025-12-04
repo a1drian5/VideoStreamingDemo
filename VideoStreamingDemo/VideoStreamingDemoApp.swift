f65f1cb4dbea52b3f365f6ae3d9108ef1fc060d9
a1drian5 <a1drian5@hotmail.com>
2025-12-03 20:33:24 -0600

    Initial Commit

---
+++ VideoStreamingDemo/VideoStreamingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoStreamingDemoApp.swift
+//  VideoStreamingDemo
+//
+//  Created by Adrian Jesus Euan Chan on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoStreamingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
